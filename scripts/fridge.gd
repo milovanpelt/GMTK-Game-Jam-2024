b3f4870f89bf4aa2126d8f3d3e85c17a3e555fda
Milo van Pelt <milovanpelt@gmail.com>
2024-08-20 01:31:08 +0200

    [ADDED] Fridge and shelves scale up

--- scripts/fridge.gd
+++ scripts/fridge.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
 const SHELVE = preload("res://scenes/shelve.tscn")
-var shelve_amount : int =  0
-var shelve_offset : int = 15
+var shelve_amount : int =  1
+var shelve_offset : int = 25
+
+var shelve_array = []
+
 
 @onready var fridge_sprite = $FridgeSprite
 
+var fridge_scale_x = 0
+var fridge_scale_y = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	fridge_scale_x =  fridge_sprite.scale.x
+	fridge_scale_y = fridge_sprite.scale.y
 	create_shelve()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,14 +25,32 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT :
-			shelve_amount += 1
+			expand_fridge()
 			create_shelve()
 			
 func create_shelve():
-	var fridge_sprite_height = fridge_sprite.texture.get_height()
-	var fridge_sprite_bottom = (fridge_sprite.position.y + fridge_sprite_height / 2)
+	remove_shelves()
+	var fridge_height = fridge_sprite.scale.y * fridge_sprite.texture.get_height()
+	var fridge_bottom = (fridge_sprite.position.y + fridge_height / 2)
 	
-	var shelve = SHELVE.instantiate()
-	shelve.position = Vector2(fridge_sprite.position.x, fridge_sprite_bottom - shelve_offset * shelve_amount)
-	add_child(shelve)
-	shelve_amount += 1
+	for i in shelve_amount:
+		var shelve = SHELVE.instantiate()
+		shelve_array.append(shelve)
+		shelve.scale.x = fridge_scale_x
+		shelve.position = Vector2(fridge_sprite.position.x, fridge_bottom - (shelve_offset * i))
+		add_child(shelve)
+	
+func remove_shelves():
+	if shelve_array.size() > 0:
+		for i in range(shelve_array.size()):
+			shelve_array[i].queue_free()
+		shelve_array.clear()
+		
+func expand_fridge():
+	if shelve_amount < 7:
+		shelve_amount += 1
+	fridge_sprite.scale.x *= 1.05
+	fridge_sprite.scale.y *= 1.05
+	
+	fridge_scale_x = fridge_sprite.scale.x
+	fridge_scale_y = fridge_sprite.scale.y
--- scripts/shelve.gd
+++ scripts/shelve.gd
@@ -14,12 +14,11 @@ func _process(delta):
 	pass
 
 func create_food():
-	var shelve_sprite_width = shelve_sprite.texture.get_width()
-	var shelve_sprite_left = (shelve_sprite.position.x - shelve_sprite_width / 2) + 10
-
+	var shelve_sprite_width = shelve_sprite.texture.get_width() * shelve_sprite.scale.x
+	var shelve_sprite_left = (shelve_sprite.position.x - shelve_sprite_width / 2) + 8
 	for i in range(food_amount):
 		var food = FRIDGE_FOOD.instantiate()
 		var min_food_position = shelve_sprite_left
 		var max_food_position = (shelve_sprite_width - food_offset) / 2
 		food.position = Vector2(randf_range(min_food_position, max_food_position), shelve_sprite.position.y - 10)
-		add_child(food)
+		add_child(food)	
